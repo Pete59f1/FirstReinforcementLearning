0e352afe6ab3a1f2b0f2620ee004f39f3ba5f38e
starup <pe7913@hotmail.com>
2020-05-09 18:24:34 +0200

    Added some methods to the script, so the agent know what actions it can take.

--- Q-Learning.py
+++ Q-Learning.py
@@ -6,11 +6,11 @@ import networkx as nx
 # Her er en liste over de valg agenten kan tage, hvor det første tal er hvor agenter står,
 # og det næste er hvor den kan gå. Kan ikke gå tilbage...
 
-
-possibleMoves = [(0, 1), (1, 5), (5, 7), (1, 6), (6, 9), (6, 2), (2, 3), (2, 8), (8, 4)]
+numberOfStates = 11
+possibleMoves = [(0, 1), (1, 5), (5, 7), (1, 6), (6, 9), (6, 2), (2, 3), (2, 8), (8, 4), (4, 10)]
 currentGoal = 9
 gamma = 0.8
-initialState = 0
+initialState = 1
 
 # En graf der viser et kort over "miljøet"
 G = nx.Graph()
@@ -24,7 +24,7 @@ plt.show()
 # Laver R vores reward matrix, og Q vores q-value matrix
 # Jeg fylder R med 1 taller, og laver dem derefter om til -1
 # Dette gøres for, at agenten for en straf for, at til en state med -1
-R = np.matrix(np.ones([10, 10]))
+R = np.matrix(np.ones([numberOfStates, numberOfStates]))
 R *= -1
 
 # Hvad der nu gøres, er at vi fjerner -1, og sætter et 0, der hvor, der er en faktisk vej, og 100 hvis det er målet.
@@ -40,10 +40,8 @@ for moves in possibleMoves:
     if moves[0] == currentGoal:
         R[moves[::-1]] = 100
     else:
-        # reverse of point
         R[moves[::-1]] = 0
 
-# loop ved målet?
 R[currentGoal, currentGoal] = 100
 
 # Hvis vi udskriver R matrixen, vil y aksen vise hvilken state vores agent er ved
@@ -51,4 +49,46 @@ R[currentGoal, currentGoal] = 100
 # Som sagt tidligere vil -1 betyde at den ikke kan gå den state
 print(R)
 
-Q = np.matrix(np.zeros([10, 10]))
+Q = np.matrix(np.zeros([numberOfStates, numberOfStates]))
+
+
+# Skriver de metoder agenten skal bruge for at tage beslutninger og lære
+# Denne metode finder hvilke muligheder agenten kan tage
+def AvailableActions(state):
+    currentStateRow = R[state, ]
+    actions = np.where(currentStateRow >= 0)[1]
+    return actions
+
+
+availableActions = AvailableActions(initialState)
+
+
+# Denne metode vælger en action ud fra de muligheder der er
+def SampleNextAction(availableActionRange):
+    nextAction = int(np.random.choice(availableActionRange, 1))
+    return nextAction
+
+
+action = SampleNextAction(availableActions)
+
+
+def Update(currentState, action, gamma):
+    maxIndex = np.where(Q[action, ] == np.max(Q[action, ]))[1]
+
+    if maxIndex.shape[0] > 1:
+        maxIndex = int(np.random.choice(maxIndex, size=1))
+    else:
+        maxIndex = int(maxIndex)
+
+    maxValue = Q[action, maxIndex]
+
+    Q[currentState, action] = R[currentState, action] + gamma * maxValue
+
+    if np.max(Q) > 0:
+        return np.sum(Q/np.max(Q) * 100)
+    else:
+        return 0
+
+
+Update(initialState, action, gamma)
+print(Q)

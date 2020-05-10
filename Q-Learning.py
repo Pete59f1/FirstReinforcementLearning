9137d36a4b9f6b559d6d6dedffccf4ce721197ff
starup <pe7913@hotmail.com>
2020-05-10 11:02:24 +0200

    Finished the script, and added some spaces between the show data, so its easier to read.

--- Q-Learning.py
+++ Q-Learning.py
@@ -2,15 +2,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 import networkx as nx
 
-# Jeg har opstillet et "miljø" hvor agenten skal fra state 0 til state 9. Dette giver os 10 states i alt.
+# Jeg har opstillet et "miljø" hvor agenten skal fra state 0 til state 9. I alt har vi 12 states.
 # Her er en liste over de valg agenten kan tage, hvor det første tal er hvor agenter står,
 # og det næste er hvor den kan gå. Kan ikke gå tilbage...
 
-numberOfStates = 11
-possibleMoves = [(0, 1), (1, 5), (5, 7), (1, 6), (6, 9), (6, 2), (2, 3), (2, 8), (8, 4), (4, 10)]
+numberOfStates = 12
+possibleMoves = [(0, 1), (1, 5), (5, 7), (1, 6), (6, 9), (6, 2), (2, 3), (2, 8), (8, 4), (4, 10), (1, 9), (9, 11)]
 currentGoal = 9
 gamma = 0.8
-initialState = 1
+initialState = 0
 
 # En graf der viser et kort over "miljøet"
 G = nx.Graph()
@@ -47,7 +47,9 @@ R[currentGoal, currentGoal] = 100
 # Hvis vi udskriver R matrixen, vil y aksen vise hvilken state vores agent er ved
 # Og x aksen viser hvilke valg den kan tage
 # Som sagt tidligere vil -1 betyde at den ikke kan gå den state
+print('')
 print(R)
+print('')
 
 Q = np.matrix(np.zeros([numberOfStates, numberOfStates]))
 
@@ -72,6 +74,7 @@ def SampleNextAction(availableActionRange):
 action = SampleNextAction(availableActions)
 
 
+# Denne metode opdaterer vores Q matrix
 def Update(currentState, action, gamma):
     maxIndex = np.where(Q[action, ] == np.max(Q[action, ]))[1]
 
@@ -91,4 +94,41 @@ def Update(currentState, action, gamma):
 
 
 Update(initialState, action, gamma)
-print(Q)
+
+# Training
+scores = []
+for i in range(1000):
+    currentState = np.random.randint(0, int(Q.shape[0]))
+    availableAction = AvailableActions(currentState)
+    action = SampleNextAction(availableAction)
+    score = Update(currentState, action, gamma)
+    scores.append(score)
+    print('Score: ', str(score))
+
+print('')
+print('Trained Q')
+print(Q/np.max(Q) * 100)
+
+# Testing
+# Her kan vi ændre hvad slutmålet er, og hvor vores agent starter
+# Herefter skulle den gerne komme med den bedst mulige vej
+currentState = 0
+steps = [currentState]
+
+while currentState != currentGoal:
+    nextStepIndex = np.where(Q[currentState, ] == np.max(Q[currentState, ]))[1]
+
+    if nextStepIndex.shape[0] > 1:
+        nextStepIndex = int(np.random.choice(nextStepIndex, size=1))
+    else:
+        nextStepIndex = int(nextStepIndex)
+
+    steps.append(nextStepIndex)
+    currentState = nextStepIndex
+
+print('')
+print('The best way')
+print(steps)
+
+plt.plot(scores)
+plt.show()
